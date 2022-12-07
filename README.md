9c3730645677cdfd6fc173e5cba12591628f75df
ilhamelmujib <ilham.elmujib@gmail.com>
2022-12-07 14:15:23 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# android-jetpack-compose-lazy-column
+# android-jetpack-compose-list-item
---
+++ app/src/main/java/id/ilhamelmujib/listitem/MainActivity.kt
@@ -0,0 +1,129 @@
+package id.ilhamelmujib.listitem
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import id.ilhamelmujib.listitem.ui.theme.ListItemTheme
+
+private val names = listOf(
+    "Maria",
+    "Gianti",
+    "Handayani",
+    "Pinter",
+    "Cantik",
+    "Baik",
+    "Manis",
+    "Lucu",
+    "Gemas",
+    "Tengil"
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListItemTheme {
+                ListItemComposeApp()
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun ListItemComposeAppPreview() {
+    ListItemComposeApp()
+}
+
+@Composable
+fun ListItemComposeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        GreetingList(names)
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val isExpanded = remember { mutableStateOf(false) }
+    val animateSizeDp by animateDpAsState(
+        targetValue = if (isExpanded.value) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            contentDescription = "Logo Jetpack Compose",
+            modifier = Modifier.size(animateSizeDp),
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(Modifier.weight(1f)) {
+            Text(
+                text = "Heyoo $name!",
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp
+            )
+            Text(text = "Welcome to Jungle")
+        }
+        IconButton(onClick = { isExpanded.value = !isExpanded.value }) {
+            Icon(
+                imageVector = if (isExpanded.value) Icons.Outlined.ExpandLess else Icons.Outlined.ExpandMore,
+                contentDescription = if (isExpanded.value) "Show less" else "Show more"
+            )
+        }
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String>) {
+    if (names.isNotEmpty()) {
+//        Column {
+//            for (name in names) {
+//                Greeting(name = name)
+//            }
+//        }
+
+        LazyColumn {
+            items(names) { name ->
+                Greeting(name)
+            }
+        }
+    } else {
+        Box(contentAlignment = Alignment.Center) {
+            Text("No people to great!")
+        }
+    }
+}
